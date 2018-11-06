8f1149ffd0a1309ae597b82dd6055361010344a7
beingofhabits <beingofhabits@gmail.com>
2018-11-06 12:26:31 +0300

    copy - paste

--- force-app/main/default/aura/test/testHelper.js
+++ force-app/main/default/aura/test/testHelper.js
@@ -1,5 +1,25 @@
 ({
     readFile : function(component, file) {
+       let fr = new FileReader();
+       
+       fr.onload = () => {
+           let fileContents = fr.result;
+           let base64Mark = `base64,`;
+           let dataStart = fileContents.indexOf(base64Mark) + base64Mark.length;
 
+           fileContents = fileContents.substring(dataStart);
+
+           this.upload(component, file, fileContents);
+       }
+       
+       fr.readAsDataURL(file);
+    }, 
+
+    upload : function(component, file, fileContents){
+        let action = component.get("v.test");
+        file = encodeURIComponent(fileContents);
+
+        action.setParams({file});
+        $A.enqueueAction(action);
     }
 })
