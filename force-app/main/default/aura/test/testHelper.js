b692413a2ad910b74889844872ec672c99880789
beingofhabits <beingofhabits@gmail.com>
2018-11-08 11:01:59 +0300

    added fileType to helper + apex

--- force-app/main/default/aura/test/testHelper.js
+++ force-app/main/default/aura/test/testHelper.js
@@ -5,21 +5,21 @@
        fr.onload = () => {
            let fileContents = fr.result,
                base64Mark = `;base64,`,
-               splittedContent = fileContents.split(base64Mark);
+               splittedContent = fileContents.split(base64Mark),
                fileType = splittedContent[0].replace(`data:`, ``); 
 
            fileContents = splittedContent[1];
-           this.upload(component, fileContents);
+           this.upload(component, fileContents, fileType);
        }
        
        fr.readAsDataURL(file);
     }, 
 
-    upload : function(component, fileContents){
+    upload : function(component, fileContents, fileType){
         let action = component.get("c.test"),
             file = encodeURIComponent(fileContents);
         console.log(`${file.length}   ${fileContents.length}`)
-        action.setParams({file});
+        action.setParams({file, fileType});
         $A.enqueueAction(action);
 
         action.setCallback(this, res => {
